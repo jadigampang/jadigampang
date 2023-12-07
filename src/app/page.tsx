244be68eba17a1279559c5e2a8bba9754ae40df9
feniafitriyani <49932839+feniafitriyani@users.noreply.github.com>
2023-12-07 15:18:24 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Home from "../component/Home/Index";
+import Nav from "../component/Navigation/Index";
+
+export default function App() {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+}
